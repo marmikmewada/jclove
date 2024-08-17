68a30dc457b6c17f3bae2125144db90654f488aa
Marmik Mewada <95102437+marmikmewada@users.noreply.github.com>
2024-08-17 20:50:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// const wrapper = document.querySelector('.wrapper');
+// const question = document.querySelector('.question');
+// const yesBtn = document.querySelector('.yes-btn');
+// const noBtn = document.querySelector('.no-btn');
+
+// const wrapperRect = wrapper.getBoundingClientRect();
+// const noBtnRect = noBtn.getBoundingClientRect();
+
+// yesBtn.addEventListener('click', () => {
+//     question.innerHTML = 'I Love You Too Jinal :)';
+// });
+
+// noBtn.addEventListener('mouseover', () => {
+//     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+//     const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+
+//     noBtn.style.left = i + 'px';
+//     noBtn.style.top = j + 'px';
+// });
+
 const wrapper = document.querySelector('.wrapper');
 const question = document.querySelector('.question');
 const yesBtn = document.querySelector('.yes-btn');
@@ -8,6 +28,8 @@ const noBtnRect = noBtn.getBoundingClientRect();
 
 yesBtn.addEventListener('click', () => {
     question.innerHTML = 'I Love You Too Jinal :)';
+    yesBtn.style.display = 'none';
+    noBtn.style.display = 'none';
 });
 
 noBtn.addEventListener('mouseover', () => {
